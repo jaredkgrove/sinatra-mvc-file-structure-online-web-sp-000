3f8b2cb158f2e95f1d3f29614588d172920c3042
Jared Grove <jaredkgrove@gmail.com>
2019-07-03 15:44:43 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,14 @@
 class Dog
   attr_accessor :name, :age, :breed
-
+  @@all
   def initialize(name, age, breed)
     @name = name
     @age = age
     @breed = breed
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
